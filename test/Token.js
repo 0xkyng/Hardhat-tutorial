eb2fef430a343895dedd92f9b66e3a8c9cffd6dc
Isaac Wanger <teryima127@gmail.com>
2023-03-20 12:59:00 +0100

    test file

--- test/Token.js
+++ test/Token.js
@@ -1,16 +1,14 @@
-const {expect} = require("chai");
-// Chai is an assertion library
+const { expect } = require("chai");
 
 describe("Token contract", function () {
-    it("Deployment should assign the total supply of the tokens to the owner", async function () {
-        const [owner] = await ethers.getSigners();
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
 
-        const Token = await ethers.getContractFactory("Token");
+    const Token = await ethers.getContractFactory("Token");
 
-        const hardhatToken = await Token.deploy();
+    const hardhatToken = await Token.deploy();
 
-        const ownerBalance = await hardhatToken.balanceof(owner.address);
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-
-    })
-})
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
